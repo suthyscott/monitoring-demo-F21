37fd3bdea92ad84a41defbca906800cb2227914e
suthyscott <sutherlandscott21@gmail.com>
2022-04-28 13:03:09 -0600

    new rollbar project token

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: 'b5ed6debab8845558cfb4f8d3bfa881e',
+  accessToken: 'fbe6a305e56a4b48941556c16ca2f4ef',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
@@ -23,29 +23,4 @@ const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
 
-// const express = require('express')
-// const path = require('path')
 
-// const app = express()
-
-// // include and initialize the rollbar library with your access token
-// var Rollbar = require('rollbar')
-// var rollbar = new Rollbar({
-//   accessToken: '7b6d4c87610f485ebf4897b15cc8fd2b',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// })
-
-// // record a generic message and send it to Rollbar
-// rollbar.log('Hello world!')
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../index.html'))
-//     rollbar.info('html file served successfully.')
-// })
-
-// const port = process.env.PORT || 4545
-
-// let students = []
-
-// app.listen(port, () => console.log(`Take us to warp ${port}!`))
