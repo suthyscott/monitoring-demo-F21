40f86f00f1d047c5aac9139905527db0ca1f4603
suthyscott <sutherlandscott21@gmail.com>
2022-04-28 13:32:48 -0600

    new info log

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('file served')
 })
 
 const port = process.env.PORT || 4545
